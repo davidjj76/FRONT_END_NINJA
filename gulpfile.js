9345d9b6d936ed4f6380910966842ab07628db51
David Jimenez <davidjj76@gmail.com>
2017-01-17 22:38:03 +0100

    Añadidos varios partials scss

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,33 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var notify = require('gulp-notify');
+var browserSync = require('browser-sync').create();
 
-gulp.task('default', function() {
+gulp.task('default', ['compile-sass'], function() {
+
+	// arrancar el servidor de browser sync
+	browserSync.init({
+		server: './'
+	});
+
 	// detecta cambios en style.scss y ejecuta la tarea compile-sass
-    gulp.watch('./src/scss/style.scss', ['compile-sass']);
+    gulp.watch('./src/scss/*.scss', ['compile-sass']);
+
+    // detecta cambios en el html
+    gulp.watch('./*.html', function() {
+    	// recarga el navegador
+    	browserSync.reload();
+    	// notificación
+    	notify().write('Browser reloaded!!!');
+    });
 });
 
 gulp.task('compile-sass', function() {
 	gulp.src('./src/scss/style.scss')					// cargo el scss
-	.pipe(sass().on('error', sass.logError))			// compilo sass
-	.pipe(gulp.dest('./dist/'));						// dejo el resultado en ./
+	.pipe(sass().on('error', function(error) {
+		return notify().write(error);
+	}))													// compilo sass
+	.pipe(gulp.dest('./dist/'))							// dejo el resultado en ./dist
+	.pipe(browserSync.stream())							// recargar css en el navegador
+	.pipe(notify('SASS Compiled!!!'));					
 });
